a8bf17beef939efe88c53aeed76ed7a89ed878fc
Akesh-Kumar1 <akumar7_be21@thapar.edu>
2023-06-13 13:02:55 +0530

    Features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "akesh@test.com",password: "PASSWORD",password_confirmation:"PASSWORD")
+10.times do |x|
+  Post.create(title:"Title#{x}",body:"Body#{x}",user_id: User.first.id)
+end
